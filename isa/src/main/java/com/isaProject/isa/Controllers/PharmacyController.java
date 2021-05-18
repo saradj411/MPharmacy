613435f775ceb25376f6af0edc23642f59df3237
saradj411 <saradjuric98@yahoo.com>
2021-05-18 23:59:27 +0200

    anotacija za frontend

--- isa/src/main/java/com/isaProject/isa/Controllers/PharmacyController.java
+++ isa/src/main/java/com/isaProject/isa/Controllers/PharmacyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value="/pharmacy")
 @Slf4j
 public class PharmacyController {
