e6b811936c313b1e432be70340f4db7b0e1aa841
saradj411 <saradjuric98@yahoo.com>
2021-05-30 17:04:51 +0200

    brisanje fronta

--- frontend-isa/src/components/Login.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<div style="background-image: url(https://image.freepik.com/free-photo/pharmacists-showing-medicine-bottle-discussing-prescription-drug-pharmacy_8087-2630.jpg);background-repeat: no-repeat;
-  
-     background-size: 150% 111%;  height: 1100px">  
-    <p>Login</p>
-<div>
-</template>
--- frontend-isa/src/components/Registration.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <p>Registration</p>
-</template>
